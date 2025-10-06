7eefd72f2040037009a1ebdc7f0ed280f3e0e324
Theo <theodordiniz@gmail.com>
2025-10-06 17:26:52 -0300

    feat: project structure

---
+++ src/test/java/com/example/tpaedsiii/TpAedsIiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpaedsiii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpAedsIiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
